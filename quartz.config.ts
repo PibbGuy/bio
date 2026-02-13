be08e4a740cacdad86591e31318080ee07c76360
Jonah Lipscomb <jonahlipscomb@gmail.com>
2026-02-13 15:47:44 -0600

    Quartz sync: Feb 13, 2026, 3:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Lexend",
         body: "Lexend",
-        code: "IBM Plex Mono",
+        code: "Lexend",
       },
       colors: {
         lightMode: {
